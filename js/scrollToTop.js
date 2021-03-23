198ad82a13ba292598d7a2f477c595ec7c4660ac
Viresh <vireshfegade@gmail.com>
2021-03-23 16:49:55 +0530

    improved scroll to top

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,34 +1,21 @@
 
-// We select the element we want to target
-var target = document.querySelector("footer");
 
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
-var rootElement = document.documentElement
+var mybutton = document.getElementById("scrollToTopBtn");
 
-// Next we want to create a function that will be called when that element is intersected
-function callback(entries, observer) {
-    // The callback will return an array of entries, even if you are only observing a single item
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            // Show button
-            scrollToTopBtn.classList.add("showBtn")
-        } else {
-            // Hide button
-            scrollToTopBtn.classList.remove("showBtn")
-        }
-    });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-function scrollToTop() {
-    rootElement.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
-scrollToTopBtn.addEventListener("click", scrollToTop);
-
-// Next we instantiate the observer with the function we created above. This takes an optional configuration
-// object that we will use in the other examples.
-let observer = new IntersectionObserver(callback);
-// Finally start observing the target element
-observer.observe(target);
+mybutton.addEventListener("click", topFunction);
