20b6f03f1c070825efe0923f2f3f35b28e931e82
Sanket Suhagiya <sanketrasik@gmail.com>
2021-04-24 20:37:05 +0530

    icons update

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,8 +1,10 @@
 var tmp = localStorage.getItem("dm");
+var dayEmo = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
+var nightEmo = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
 
 if(tmp == null) {
 	localStorage.setItem("dm", "day");
-	document.getElementById("darkModeBtn").innerHTML = "🌞";
+	document.getElementById("darkModeBtn").innerHTML = nightEmo;
 }
 
 var elementsList = [];
@@ -11,12 +13,12 @@ elementsList.push(document.getElementsByClassName("single-person"));
 elementsList.push(document.getElementsByClassName("card-deck"));
 elementsList.push(document.getElementsByClassName("timeline-card"));
 // elementsList.push(document.getElementsByTagName("p"));
-// elementsList.push(document.getElementsByClassName("edu-icons"));
+elementsList.push(document.getElementsByClassName("formContainer"));
 // elementsList.push(document.getElementsByClassName("card-img-top"));
 elementsList.push(document.getElementsByTagName("body"));
 
 if(tmp == "night"){
-	document.getElementById("darkModeBtn").innerHTML = "🌚";
+	document.getElementById("darkModeBtn").innerHTML = dayEmo;
 	for(var i = 0; i < elementsList.length; i++) {
 		for(var j = 0; j < elementsList[i].length; j++) {
 			elementsList[i][j].classList.add("dark-mode");
@@ -25,11 +27,12 @@ if(tmp == "night"){
 }
 	
 else{
-	for(var i = 0; i < elementsList.length; i++) {
-		for(var j = 0; j < elementsList[i].length; j++) {
-			elementsList[i][j].classList.remove("dark-mode");
-		}
-	}
+	document.getElementById("darkModeBtn").innerHTML = nightEmo;
+	// for(var i = 0; i < elementsList.length; i++) {
+		// for(var j = 0; j < elementsList[i].length; j++) {
+			// elementsList[i][j].classList.remove("dark-mode");
+		// }
+	// }
 }
 
 
@@ -38,7 +41,7 @@ function darker() {
 	
 	if(tmp == "day"){
 		localStorage.setItem("dm", "night");
-		document.getElementById("darkModeBtn").innerHTML = "🌚";
+		document.getElementById("darkModeBtn").innerHTML = dayEmo;
 		for(var i = 0; i < elementsList.length; i++) {
 			for(var j = 0; j < elementsList[i].length; j++) {
 				elementsList[i][j].classList.add("dark-mode");
@@ -48,7 +51,7 @@ function darker() {
 	
 	else{
 		localStorage.setItem("dm", "day");
-		document.getElementById("darkModeBtn").innerHTML = "🌞";
+		document.getElementById("darkModeBtn").innerHTML = nightEmo;
 		for(var i = 0; i < elementsList.length; i++) {
 			for(var j = 0; j < elementsList[i].length; j++) {
 				elementsList[i][j].classList.remove("dark-mode");
