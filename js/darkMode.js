b7dc9fe8bf95d706c0efbaa7fde1a220495be477
Sanket Suhagiya <sanketrasik@gmail.com>
2021-04-24 18:14:33 +0530

    refined dark mode

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,11 +1,33 @@
 var tmp = localStorage.getItem("dm");
-	
+
+if(tmp == null) {
+	localStorage.setItem("dm", "day");
+}
+
+var elementsList = [];
+elementsList.push(document.getElementsByClassName("card"));
+elementsList.push(document.getElementsByClassName("single-person"));
+elementsList.push(document.getElementsByClassName("card-deck"));
+elementsList.push(document.getElementsByClassName("timeline-card"));
+// elementsList.push(document.getElementsByTagName("p"));
+// elementsList.push(document.getElementsByClassName("edu-icons"));
+// elementsList.push(document.getElementsByClassName("card-img-top"));
+elementsList.push(document.getElementsByTagName("body"));
+
 if(tmp == "night"){
-	document.body.classList.add("dark-mode");
+	for(var i = 0; i < elementsList.length; i++) {
+		for(var j = 0; j < elementsList[i].length; j++) {
+			elementsList[i][j].classList.add("dark-mode");
+		}
+	}
 }
 	
 else{
-	document.body.classList.remove("dark-mode");
+	for(var i = 0; i < elementsList.length; i++) {
+		for(var j = 0; j < elementsList[i].length; j++) {
+			elementsList[i][j].classList.remove("dark-mode");
+		}
+	}
 }
 
 
@@ -14,12 +36,20 @@ function darker() {
 	
 	if(tmp == "day"){
 		localStorage.setItem("dm", "night");
-		document.body.classList.add("dark-mode");
+		for(var i = 0; i < elementsList.length; i++) {
+			for(var j = 0; j < elementsList[i].length; j++) {
+				elementsList[i][j].classList.add("dark-mode");
+			}
+		}
 	}
 	
 	else{
 		localStorage.setItem("dm", "day");
-		document.body.classList.remove("dark-mode");
+		for(var i = 0; i < elementsList.length; i++) {
+			for(var j = 0; j < elementsList[i].length; j++) {
+				elementsList[i][j].classList.remove("dark-mode");
+			}
+		}
 	}
 
 }
