dbc629b8d11a2457246890603c801f9071ef918b
Viresh <vireshfegade@gmail.com>
2021-09-23 17:48:49 +0530

    use fetch for reading json data

--- js/loadTeam.js
+++ js/loadTeam.js
@@ -1,72 +1,72 @@
-function Get(yourUrl){
-    var Httpreq = new XMLHttpRequest();
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;          
-}
+
+var json_obj;
 
 
-var json_obj = JSON.parse(Get("https://raw.githubusercontent.com/viru2001/SolecthonWebsite/master/data.json"));
-//console.log("this is the author name: "+json_obj.currentMembers);
+const  fillData = async() => {
+  await fetch('https://raw.githubusercontent.com/viru2001/SolecthonWebsite/master/data.json')
+  .then(res => res.json())
+  .then(json => {
+    json_obj = json;
+  });
+  
+  let iDiv = document.createElement("div");
+  iDiv.id = "currentMembers";
+  iDiv.className = "row";
 
+  let iDiv2 = document.createElement("div");
+  iDiv2.id = "oldMembers";
+  iDiv2.className = "row";
 
+  let currentMemberDiv = document.getElementsByClassName("container")[0];
+  currentMemberDiv.appendChild(iDiv);
+  let oldMemberDiv = document.getElementsByClassName("container")[1];
+  oldMemberDiv.appendChild(iDiv2);
 
-var iDiv = document.createElement('div');
-iDiv.id = 'currentMembers';
-iDiv.className = 'row';
+  let divs = [iDiv, iDiv2];
+  let json_objs = [json_obj.currentMembers, json_obj.oldMembers];
 
-var iDiv2 = document.createElement('div');
-iDiv2.id = 'oldMembers';
-iDiv2.className = 'row';
+  for (let j = 0; j < divs.length; j++) {
+    for (let i = 0; i < json_objs[j].length; i++) {
+      let innerDiv = document.createElement("div");
+      innerDiv.className = "col-sm-6 col-lg-4 col-xl-3";
+      divs[j].appendChild(innerDiv);
 
-var currentMemberDiv = document.getElementsByClassName('container')[0];
-currentMemberDiv.appendChild(iDiv);
-var oldMemberDiv = document.getElementsByClassName('container')[1];
-oldMemberDiv.appendChild(iDiv2);
+      let personDiv = document.createElement("div");
+      personDiv.className = "single-person";
+      innerDiv.appendChild(personDiv);
 
-var divs = [iDiv, iDiv2]
-var json_objs = [json_obj.currentMembers, json_obj.oldMembers]
+      let imageDiv = document.createElement("div");
+      imageDiv.className = "person-image";
+      personDiv.appendChild(imageDiv);
 
-for(var j = 0; j < divs.length; j++) {
-	for(var i = 0; i < json_objs[j].length; i++) {
-		var innerDiv = document.createElement('div');
-		innerDiv.className = 'col-sm-6 col-lg-4 col-xl-3';
-		divs[j].appendChild(innerDiv);
-		
-		var personDiv = document.createElement('div');
-		personDiv.className = 'single-person';
-		innerDiv.appendChild(personDiv);
-		
-		var imageDiv = document.createElement('div');
-		imageDiv.className = 'person-image';
-		personDiv.appendChild(imageDiv);
-		
-		var imgTag = document.createElement('img');
-		imgTag.src = json_objs[j][i].image;
-		imgTag.loading="lazy";
-		imageDiv.appendChild(imgTag);
-		
-		var icon = document.createElement('span');
-		icon.className = 'icon'
-		imageDiv.append(icon);
-		
-		var icon2 = document.createElement('i');
-		icon2.className = json_objs[j][i].icon;
-		icon.appendChild(icon2);
-		
-		var personInfo = document.createElement('div');
-		personInfo.className = 'person-info';
-		personDiv.appendChild(personInfo);
-		
-		var hthree = document.createElement('h3');
-		hthree.className = 'full-name'
-		hthree.innerHTML = json_objs[j][i].name;
-		personInfo.append(hthree);
-		
-		var spec = document.createElement('span');
-		spec.className = 'speciality'
-		spec.innerHTML = json_objs[j][i].speciality;
-		personInfo.append(spec);
+      let imgTag = document.createElement("img");
+      imgTag.src = json_objs[j][i].image;
+      imgTag.loading = "lazy";
+      imageDiv.appendChild(imgTag);
 
-	}
-}
+      let icon = document.createElement("span");
+      icon.className = "icon";
+      imageDiv.append(icon);
+
+      let icon2 = document.createElement("i");
+      icon2.className = json_objs[j][i].icon;
+      icon.appendChild(icon2);
+
+      let personInfo = document.createElement("div");
+      personInfo.className = "person-info";
+      personDiv.appendChild(personInfo);
+
+      let hthree = document.createElement("h3");
+      hthree.className = "full-name";
+      hthree.innerHTML = json_objs[j][i].name;
+      personInfo.append(hthree);
+
+      let spec = document.createElement("span");
+      spec.className = "speciality";
+      spec.innerHTML = json_objs[j][i].speciality;
+      personInfo.append(spec);
+    }
+  }
+};
+
+fillData();
