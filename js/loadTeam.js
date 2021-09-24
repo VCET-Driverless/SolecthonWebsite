4d789f0313bad541656271278d41060c2430579d
Viresh <vireshfegade@gmail.com>
2021-09-24 22:03:23 +0530

    add dynamic loading of team members

--- js/loadTeam.js
+++ js/loadTeam.js
@@ -9,27 +9,53 @@ const fillData = async () => {
       json_obj = json;
     });
 
-  let iDiv = document.createElement("div");
-  iDiv.id = "currentMembers";
-  iDiv.className = "row";
+  let teamsKeys = Object.keys(json_obj);
+  console.log(teamsKeys);
 
-  let iDiv2 = document.createElement("div");
-  iDiv2.id = "oldMembers";
-  iDiv2.className = "row";
+  let teams = document.querySelector("#teams");
 
-  let currentMemberDiv = document.getElementsByClassName("container")[0];
-  currentMemberDiv.appendChild(iDiv);
-  let oldMemberDiv = document.getElementsByClassName("container")[1];
-  oldMemberDiv.appendChild(iDiv2);
+  teamsKeys.map((x) => {
+    console.log(x);
 
-  let divs = [iDiv, iDiv2];
-  let json_objs = [json_obj.currentMembers, json_obj.oldMembers];
+    let heading = document.createElement("h1");
+    heading.innerText = x;
 
-  for (let j = 0; j < divs.length; j++) {
-    for (let i = 0; i < json_objs[j].length; i++) {
+    let centerDiv = document.createElement("div");
+    centerDiv.className = "centered";
+    centerDiv.appendChild(heading);
+
+    let mainDiv = document.createElement("div");
+    mainDiv.className = "containerTop header1";
+    mainDiv.appendChild(centerDiv);
+    teams.appendChild(mainDiv);
+
+
+
+    
+
+    let teamSection = document.createElement("section");
+    teamSection.className = "section-team";
+    teams.appendChild(teamSection);
+
+    let sectionDiv = document.createElement("div");
+    sectionDiv.className = "container";
+    teamSection.appendChild(sectionDiv);
+
+    let newDiv = document.createElement("div");
+    newDiv.id = x;
+    newDiv.className = "row";
+
+    sectionDiv.appendChild(newDiv);
+    
+    
+
+   
+
+    for (let i = 0; i < json_obj[x].length; i++) {
+      console.log(json_obj[x][i]);
       let innerDiv = document.createElement("div");
       innerDiv.className = "col-sm-6 col-lg-4 col-xl-3";
-      divs[j].appendChild(innerDiv);
+      newDiv.appendChild(innerDiv);
 
       let personDiv = document.createElement("div");
       personDiv.className = "single-person";
@@ -40,7 +66,7 @@ const fillData = async () => {
       personDiv.appendChild(imageDiv);
 
       let imgTag = document.createElement("img");
-      imgTag.src = json_objs[j][i].image;
+      imgTag.src = json_obj[x][i].image;
       imgTag.loading = "lazy";
       imageDiv.appendChild(imgTag);
 
@@ -49,7 +75,7 @@ const fillData = async () => {
       imageDiv.append(icon);
 
       let icon2 = document.createElement("i");
-      icon2.className = json_objs[j][i].icon;
+      icon2.className = json_obj[x][i].icon;
       icon.appendChild(icon2);
 
       let personInfo = document.createElement("div");
@@ -58,15 +84,76 @@ const fillData = async () => {
 
       let hthree = document.createElement("h3");
       hthree.className = "full-name";
-      hthree.innerHTML = json_objs[j][i].name;
+      hthree.innerHTML = json_obj[x][i].name;
       personInfo.append(hthree);
 
       let spec = document.createElement("span");
       spec.className = "speciality";
-      spec.innerHTML = json_objs[j][i].role;
+      spec.innerHTML = json_obj[x][i].role;
       personInfo.append(spec);
+
+      // sectionDiv.appendChild(innerDiv);
     }
-  }
+  });
+
+  // let iDiv = document.createElement("div");
+  // iDiv.id = "currentMembers";
+  // iDiv.className = "row";
+
+  // let iDiv2 = document.createElement("div");
+  // iDiv2.id = "oldMembers";
+  // iDiv2.className = "row";
+
+  // let currentMemberDiv = document.getElementsByClassName("container")[0];
+  // currentMemberDiv.appendChild(iDiv);
+  // let oldMemberDiv = document.getElementsByClassName("container")[1];
+  // oldMemberDiv.appendChild(iDiv2);
+
+  // let divs = [iDiv, iDiv2];
+  // let json_objs = [json_obj.currentMembers, json_obj.oldMembers];
+
+  // for (let j = 0; j < divs.length; j++) {
+  //   for (let i = 0; i < json_objs[j].length; i++) {
+  //     let innerDiv = document.createElement("div");
+  //     innerDiv.className = "col-sm-6 col-lg-4 col-xl-3";
+  //     divs[j].appendChild(innerDiv);
+
+  //     let personDiv = document.createElement("div");
+  //     personDiv.className = "single-person";
+  //     innerDiv.appendChild(personDiv);
+
+  //     let imageDiv = document.createElement("div");
+  //     imageDiv.className = "person-image";
+  //     personDiv.appendChild(imageDiv);
+
+  //     let imgTag = document.createElement("img");
+  //     imgTag.src = json_objs[j][i].image;
+  //     imgTag.loading = "lazy";
+  //     imageDiv.appendChild(imgTag);
+
+  //     let icon = document.createElement("span");
+  //     icon.className = "icon";
+  //     imageDiv.append(icon);
+
+  //     let icon2 = document.createElement("i");
+  //     icon2.className = json_objs[j][i].icon;
+  //     icon.appendChild(icon2);
+
+  //     let personInfo = document.createElement("div");
+  //     personInfo.className = "person-info";
+  //     personDiv.appendChild(personInfo);
+
+  //     let hthree = document.createElement("h3");
+  //     hthree.className = "full-name";
+  //     hthree.innerHTML = json_objs[j][i].name;
+  //     personInfo.append(hthree);
+
+  //     let spec = document.createElement("span");
+  //     spec.className = "speciality";
+  //     spec.innerHTML = json_objs[j][i].role;
+  //     personInfo.append(spec);
+  //   }
+  // }
   let tmp = localStorage.getItem("dm");
 
   if (tmp == null) {
