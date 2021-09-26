c849a3d0655d57a2908b8c9f3b8c417fa16f1449
Viresh <vireshfegade@gmail.com>
2021-09-26 22:59:19 +0530

    update team.json and old members images

--- js/loadTeam.js
+++ js/loadTeam.js
@@ -66,7 +66,9 @@ const fillData = async () => {
       personDiv.appendChild(imageDiv);
 
       let imgTag = document.createElement("img");
-      imgTag.src = "https://raw.githubusercontent.com/viru2001/SolecthonWebsite/master/images/members/"+ json_obj[x][i].image;
+      // imgTag.src = "https://raw.githubusercontent.com/viru2001/SolecthonWebsite/master/images/members/"+ json_obj[x][i].image;
+      imgTag.src = "images/members/"+ json_obj[x][i].image;
+      
       imgTag.loading = "lazy";
       imageDiv.appendChild(imgTag);
 
@@ -96,64 +98,7 @@ const fillData = async () => {
     }
   });
 
-  // let iDiv = document.createElement("div");
-  // iDiv.id = "currentMembers";
-  // iDiv.className = "row";
-
-  // let iDiv2 = document.createElement("div");
-  // iDiv2.id = "oldMembers";
-  // iDiv2.className = "row";
-
-  // let currentMemberDiv = document.getElementsByClassName("container")[0];
-  // currentMemberDiv.appendChild(iDiv);
-  // let oldMemberDiv = document.getElementsByClassName("container")[1];
-  // oldMemberDiv.appendChild(iDiv2);
-
-  // let divs = [iDiv, iDiv2];
-  // let json_objs = [json_obj.currentMembers, json_obj.oldMembers];
-
-  // for (let j = 0; j < divs.length; j++) {
-  //   for (let i = 0; i < json_objs[j].length; i++) {
-  //     let innerDiv = document.createElement("div");
-  //     innerDiv.className = "col-sm-6 col-lg-4 col-xl-3";
-  //     divs[j].appendChild(innerDiv);
-
-  //     let personDiv = document.createElement("div");
-  //     personDiv.className = "single-person";
-  //     innerDiv.appendChild(personDiv);
-
-  //     let imageDiv = document.createElement("div");
-  //     imageDiv.className = "person-image";
-  //     personDiv.appendChild(imageDiv);
-
-  //     let imgTag = document.createElement("img");
-  //     imgTag.src = json_objs[j][i].image;
-  //     imgTag.loading = "lazy";
-  //     imageDiv.appendChild(imgTag);
-
-  //     let icon = document.createElement("span");
-  //     icon.className = "icon";
-  //     imageDiv.append(icon);
-
-  //     let icon2 = document.createElement("i");
-  //     icon2.className = json_objs[j][i].icon;
-  //     icon.appendChild(icon2);
-
-  //     let personInfo = document.createElement("div");
-  //     personInfo.className = "person-info";
-  //     personDiv.appendChild(personInfo);
-
-  //     let hthree = document.createElement("h3");
-  //     hthree.className = "full-name";
-  //     hthree.innerHTML = json_objs[j][i].name;
-  //     personInfo.append(hthree);
-
-  //     let spec = document.createElement("span");
-  //     spec.className = "speciality";
-  //     spec.innerHTML = json_objs[j][i].role;
-  //     personInfo.append(spec);
-  //   }
-  // }
+  
   let tmp = localStorage.getItem("dm");
 
   if (tmp == null) {
